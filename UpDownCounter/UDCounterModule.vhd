122d867f5527f1fb432afa7e6f335e023de8c118
Charlesthiem <charlesthiem@gmail.com>
2017-04-22 21:33:02 -0400

    Add files via upload
    test

--- UpDownCounter/UDCounterModule.vhd
+++ UpDownCounter/UDCounterModule.vhd
@@ -1,39 +1,12 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    18:23:59 03/28/2017 
--- Design Name: 
--- Module Name:    UDCounterModule - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity UDCounterModule is
-  port(Cin,Clk,PE,Res,UD: in std_logic;
-       P: in std_logic_vector(1 to 4);
+  port(Cin,Clk,PE,reset,UD: in std_logic;
+       P: in std_logic_vector(4 downto 1);
        Cout: out std_logic;
-       Q: out std_logic_vector(1 to 4));
+       Q: out std_logic_vector(4 downto 1));
 end UDCounterModule;
 
 architecture Behavioral of UDCounterModule is
@@ -51,23 +24,28 @@ begin
     elsif (UD = '0' and Q = "0000") then
       Cout <= '0';
     else
-      Cout = '1';
+      Cout <= '1';
     end if;
   end process;
       
-  SetQ: process(Cin,PE,Res,Clk) is
+  SetQ: process(Cin,PE,Reset,Clk) is
     
   begin
-    if Res = '1' then
+    if Reset = '1' then
       Q <= "0000";
     elsif PE = '1' then
       Q <= P;
     elsif Cin = '1' then
       null;
     elsif rising_edge(Clk) = '1' then
-      
+      if UD = '1' then
+			Q<=Q+1; -- if UD is high count up
+						-- code for when a pe out of range is given
+		else
+			Q<=Q-1; -- if UD is low
+						-- code for when pe out of range
+		end if;
     end if;
   end process;
       
 end Behavioral;
-
